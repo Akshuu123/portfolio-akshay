82fe5cee648d93a9312fa59cc7f8b2b90ca97212
akshayfanatic <akshay@fanaticcoders.com>
2025-08-02 16:48:16 +0530

    feat: setup my  project add, support of prettier,semver ,shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,20 +8,33 @@ export default function Home() {
           className="dark:invert"
           src="/next.svg"
           alt="Next.js logo"
-          width={180}
-          height={38}
+          width={
+            180
+          }
+          height={
+            38
+          }
           priority
         />
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
+            Get
+            started
+            by
+            editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
               src/app/page.tsx
             </code>
+
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+            Save
+            and
+            see
+            your
+            changes
+            instantly.
           </li>
         </ol>
 
@@ -36,10 +49,15 @@ export default function Home() {
               className="dark:invert"
               src="/vercel.svg"
               alt="Vercel logomark"
-              width={20}
-              height={20}
+              width={
+                20
+              }
+              height={
+                20
+              }
             />
-            Deploy now
+            Deploy
+            now
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
@@ -47,7 +65,9 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            Read
+            our
+            docs
           </a>
         </div>
       </main>
@@ -62,8 +82,12 @@ export default function Home() {
             aria-hidden
             src="/file.svg"
             alt="File icon"
-            width={16}
-            height={16}
+            width={
+              16
+            }
+            height={
+              16
+            }
           />
           Learn
         </a>
@@ -77,8 +101,12 @@ export default function Home() {
             aria-hidden
             src="/window.svg"
             alt="Window icon"
-            width={16}
-            height={16}
+            width={
+              16
+            }
+            height={
+              16
+            }
           />
           Examples
         </a>
@@ -92,10 +120,17 @@ export default function Home() {
             aria-hidden
             src="/globe.svg"
             alt="Globe icon"
-            width={16}
-            height={16}
+            width={
+              16
+            }
+            height={
+              16
+            }
           />
-          Go to nextjs.org →
+          Go
+          to
+          nextjs.org
+          →
         </a>
       </footer>
     </div>
